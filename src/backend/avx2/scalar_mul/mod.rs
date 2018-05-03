9d43623ce501c198d70fa91d312d340a375cdedc
Henry de Valence <hdevalence@hdevalence.ca>
2018-05-02 17:20:58 -0700

    Use multiscalar traits for the backend implementations.

--- src/backend/avx2/scalar_mul/mod.rs
+++ src/backend/avx2/scalar_mul/mod.rs
@@ -8,13 +8,102 @@
 // - Isis Agora Lovecruft <isis@patternsinthevoid.net>
 // - Henry de Valence <hdevalence@hdevalence.ca>
 
+use core::borrow::Borrow;
+
+use clear_on_drop::ClearOnDrop;
+
+use backend::avx2::edwards::{CachedPoint, ExtendedPoint};
+use edwards::EdwardsPoint;
+use scalar::Scalar;
+use scalar_mul::window::LookupTable;
+use traits::Identity;
+
 pub mod variable_base;
 
-#[cfg(feature="precomputed_tables")]
+#[cfg(feature = "precomputed_tables")]
 pub mod vartime_double_base;
 
-#[cfg(any(feature = "alloc", feature = "std"))]
-pub mod straus;
+/// Multiscalar multiplication using interleaved window / Straus'
+/// method.  See the `Straus` struct in the serial backend for more
+/// details.
+///
+/// This exists as a seperate implementation from that one because the
+/// AVX2 code uses different curve models (it does not pass between
+/// multiple models during scalar mul), and it has to convert the
+/// point representation on the fly.
+pub struct Straus {}
 
 #[cfg(any(feature = "alloc", feature = "std"))]
-pub mod vartime_straus;
+impl MultiscalarMul for Straus {
+    type Point = EdwardsPoint;
+
+    fn multiscalar_mul<I, J>(scalars: I, points: J) -> EdwardsPoint
+    where
+        I: IntoIterator,
+        I::Item: Borrow<Scalar>,
+        J: IntoIterator,
+        J::Item: Borrow<EdwardsPoint>,
+    {
+        // Construct a lookup table of [P,2P,3P,4P,5P,6P,7P,8P]
+        // for each input point P
+        let lookup_tables: Vec<_> = points
+            .into_iter()
+            .map(|point| LookupTable::<CachedPoint>::from(point.borrow()))
+            .collect();
+
+        let scalar_digits_vec: Vec<_> = scalars
+            .into_iter()
+            .map(|s| s.borrow().to_radix_16())
+            .collect();
+        // Pass ownership to a ClearOnDrop wrapper
+        let scalar_digits = ClearOnDrop::new(scalar_digits_vec);
+
+        let mut Q = ExtendedPoint::identity();
+        for j in (0..64).rev() {
+            Q = Q.mul_by_pow_2(4);
+            let it = scalar_digits.iter().zip(lookup_tables.iter());
+            for (s_i, lookup_table_i) in it {
+                // Q = Q + s_{i,j} * P_i
+                Q = &Q + &lookup_table_i.select(s_i[j]);
+            }
+        }
+        Q.into()
+    }
+}
+
+#[cfg(any(feature = "alloc", feature = "std"))]
+impl VartimeMultiscalarMul for Straus {
+    type Point = EdwardsPoint;
+
+    fn vartime_multiscalar_mul<I, J>(scalars: I, points: J) -> EdwardsPoint
+    where
+        I: IntoIterator,
+        I::Item: Borrow<Scalar>,
+        J: IntoIterator,
+        J::Item: Borrow<EdwardsPoint>,
+    {
+        let nafs: Vec<_> = scalars
+            .into_iter()
+            .map(|c| c.borrow().non_adjacent_form(5))
+            .collect();
+        let lookup_tables: Vec<_> = points
+            .into_iter()
+            .map(|point| NafLookupTable5::<CachedPoint>::from(point.borrow()))
+            .collect();
+
+        let mut Q = ExtendedPoint::identity();
+
+        for i in (0..255).rev() {
+            Q = Q.double();
+
+            for (naf, lookup_table) in nafs.iter().zip(lookup_tables.iter()) {
+                if naf[i] > 0 {
+                    Q = &Q + &lookup_table.select(naf[i] as usize);
+                } else if naf[i] < 0 {
+                    Q = &Q - &lookup_table.select(-naf[i] as usize);
+                }
+            }
+        }
+        Q.into()
+    }
+}
