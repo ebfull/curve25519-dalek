e8715abce2ad15c41b98b7adc3018c3a28c6ede3
Henry de Valence <hdevalence@hdevalence.ca>
2018-05-07 10:53:32 -0700

    WIP add corresponding code to AVX2 backend

--- src/backend/avx2/scalar_mul/mod.rs
+++ src/backend/avx2/scalar_mul/mod.rs
@@ -15,8 +15,8 @@ use clear_on_drop::ClearOnDrop;
 use backend::avx2::edwards::{CachedPoint, ExtendedPoint};
 use edwards::EdwardsPoint;
 use scalar::Scalar;
-use scalar_mul::window::LookupTable;
-use traits::Identity;
+use scalar_mul::window::{LookupTable, NafLookupTable5, NafLookupTable8};
+use traits::*;
 
 pub mod variable_base;
 
@@ -104,6 +104,167 @@ impl VartimeMultiscalarMul for Straus {
                 }
             }
         }
+        Q.into()
+    }
+}
+
+pub struct PrecomputedStraus {
+    lookup_tables: Vec<LookupTable<CachedPoint>>,
+}
+
+#[cfg(any(feature = "alloc", feature = "std"))]
+impl PrecomputedMultiscalarMul for PrecomputedStraus {
+    type Point = EdwardsPoint;
+
+    fn new<I>(static_points: I) -> Self
+    where
+        I: IntoIterator,
+        I::Item: Borrow<Self::Point>,
+    {
+        PrecomputedStraus {
+            lookup_tables: static_points
+                .into_iter()
+                .map(|point| LookupTable::<CachedPoint>::from(point.borrow()))
+                .collect(),
+        }
+    }
+
+    fn mixed_multiscalar_mul<I, J, K>(
+        &self,
+        static_scalars: I,
+        dynamic_scalars: J,
+        dynamic_points: K,
+    ) -> Self::Point
+    where
+        I: IntoIterator,
+        I::Item: Borrow<Scalar>,
+        J: IntoIterator,
+        J::Item: Borrow<Scalar>,
+        K: IntoIterator,
+        K::Item: Borrow<Self::Point>,
+    {
+        // Compute the scalar digits for the static and dynamic scalars.
+        // To ensure that these are erased, pass ownership of the Vec into a
+        // ClearOnDrop wrapper.
+
+        let static_scalar_digits_vec: Vec<_> = static_scalars
+            .into_iter()
+            .map(|s| s.borrow().to_radix_16())
+            .collect();
+        let static_scalar_digits = ClearOnDrop::new(static_scalar_digits_vec);
+
+        let dynamic_scalar_digits_vec: Vec<_> = dynamic_scalars
+            .into_iter()
+            .map(|s| s.borrow().to_radix_16())
+            .collect();
+        let dynamic_scalar_digits = ClearOnDrop::new(dynamic_scalar_digits_vec);
+
+        let dynamic_lookup_tables: Vec<_> = dynamic_points
+            .into_iter()
+            .map(|point| LookupTable::<CachedPoint>::from(point.borrow()))
+            .collect();
+
+        let mut Q = ExtendedPoint::identity();
+        for j in (0..64).rev() {
+            Q = Q.mul_by_pow_2(4);
+
+            // Add the static points
+            let it = static_scalar_digits.iter().zip(self.lookup_tables.iter());
+            for (s_i, lookup_table_i) in it {
+                // Q = Q + s_{i,j} * P_i
+                Q = &Q + &lookup_table_i.select(s_i[j]);
+            }
+
+            // Add the dynamic points
+            let it = dynamic_scalar_digits
+                .iter()
+                .zip(dynamic_lookup_tables.iter());
+            for (s_i, lookup_table_i) in it {
+                // Q = Q + s_{i,j} * P_i
+                Q = &Q + &lookup_table_i.select(s_i[j]);
+            }
+        }
+
+        Q.into()
+    }
+}
+
+pub struct VartimePrecomputedStraus {
+    lookup_tables: Vec<NafLookupTable8<CachedPoint>>,
+}
+
+#[cfg(any(feature = "alloc", feature = "std"))]
+impl VartimePrecomputedMultiscalarMul for VartimePrecomputedStraus {
+    type Point = EdwardsPoint;
+
+    fn new<I>(static_points: I) -> Self
+    where
+        I: IntoIterator,
+        I::Item: Borrow<Self::Point>,
+    {
+        VartimePrecomputedStraus {
+            lookup_tables: static_points
+                .into_iter()
+                .map(|point| NafLookupTable8::<CachedPoint>::from(point.borrow()))
+                .collect(),
+        }
+    }
+
+    fn vartime_mixed_multiscalar_mul<I, J, K>(
+        &self,
+        static_scalars: I,
+        dynamic_scalars: J,
+        dynamic_points: K,
+    ) -> Self::Point
+    where
+        I: IntoIterator,
+        I::Item: Borrow<Scalar>,
+        J: IntoIterator,
+        J::Item: Borrow<Scalar>,
+        K: IntoIterator,
+        K::Item: Borrow<Self::Point>,
+    {
+        let static_scalar_nafs: Vec<_> = static_scalars
+            .into_iter()
+            .map(|c| c.borrow().non_adjacent_form(8))
+            .collect();
+
+        let dynamic_scalar_nafs: Vec<_> = dynamic_scalars
+            .into_iter()
+            .map(|c| c.borrow().non_adjacent_form(5))
+            .collect();
+
+        let dynamic_lookup_tables: Vec<_> = dynamic_points
+            .into_iter()
+            .map(|P| NafLookupTable5::<CachedPoint>::from(P.borrow()))
+            .collect();
+
+        let mut Q = ExtendedPoint::identity();
+
+        for i in (0..255).rev() {
+            Q = Q.double();
+
+            // Static points use width-8 NAFs
+            let it = static_scalar_nafs.iter().zip(self.lookup_tables.iter());
+            for (naf, lookup_table) in it {
+                if naf[i] > 0 {
+                    Q = &Q + &lookup_table.select(naf[i] as usize);
+                } else if naf[i] < 0 {
+                    Q = &Q - &lookup_table.select(-naf[i] as usize);
+                }
+            }
+
+            // Dynamic points use width-5 NAFs
+            let it = dynamic_scalar_nafs.iter().zip(dynamic_lookup_tables.iter());
+            for (naf, lookup_table) in it {
+                if naf[i] > 0 {
+                    Q = &Q + &lookup_table.select(naf[i] as usize);
+                } else if naf[i] < 0 {
+                    Q = &Q - &lookup_table.select(-naf[i] as usize);
+                }
+            }
+        }
+
         Q.into()
     }
 }
